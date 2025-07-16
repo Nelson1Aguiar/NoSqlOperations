98c4986092d92842e066da1e884a24a7b4b465e3
Nelson <nelsonsales20@hotmail.com>
2025-07-16 15:50:43 -0300

    fix: Interface de RedisOperations corrigida

--- Interfaces/IRedisOperation.cs
+++ Interfaces/IRedisOperation.cs
@@ -4,6 +4,8 @@
     {
         public void SetData<T>(T entity, string redisKey);
         public T GetData<T>(string redisKey) where T : class, new();
-        public List<T> GetAllDataByKey<T>(string redisKey);
+        public List<T> GetAllDataByKeyPattern<T>(string redisKey);
+        public void DeleteByKey(string redisKey);
+        public void DeleteAllByKeyPattern(string redisKey);
     }
 }
